fc98edbbd371ea19c1e8df570ca62537c66db2f1
Jonsen Gaire <jonsen455@gmail.com>
2025-11-07 14:03:14 +0545

    Model classes

---
+++ lib/models/person.dart
@@ -0,0 +1,23 @@
+import 'package:hive/hive.dart';
+
+part 'person.g.dart';
+@HiveType(typeId: 0)
+class Person extends HiveObject{
+
+  @HiveField(0)
+  String name;
+  @HiveField(1)
+  String email;
+  @HiveField(2)
+  int phone;
+  @HiveField(3)
+  String? address;
+  @HiveField(4)
+  String? description;
+  @HiveField(5)
+  String? image;
+
+
+  Person({required this.name, required this.email, required this.phone, this.address, this.description, this.image});
+
+}
---
+++ lib/models/person.g.dart
@@ -0,0 +1,56 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'person.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class PersonAdapter extends TypeAdapter<Person> {
+  @override
+  final int typeId = 0;
+
+  @override
+  Person read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Person(
+      name: fields[0] as String,
+      email: fields[1] as String,
+      phone: fields[2] as int,
+      address: fields[3] as String?,
+      description: fields[4] as String?,
+      image: fields[5] as String?,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Person obj) {
+    writer
+      ..writeByte(6)
+      ..writeByte(0)
+      ..write(obj.name)
+      ..writeByte(1)
+      ..write(obj.email)
+      ..writeByte(2)
+      ..write(obj.phone)
+      ..writeByte(3)
+      ..write(obj.address)
+      ..writeByte(4)
+      ..write(obj.description)
+      ..writeByte(5)
+      ..write(obj.image);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is PersonAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
