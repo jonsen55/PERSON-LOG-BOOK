a70ad0af7fdf4e57125c8ecd14acb216c73853fe
Jonsen Gaire <jonsen455@gmail.com>
2025-11-10 17:33:03 +0545

    Main class ho ni feri

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:hive_third/view/home_screen.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:hive/hive.dart';
+import './models/person.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  var directory = await getApplicationDocumentsDirectory();
+
+  Hive.init(directory.path);
+
+  Hive.registerAdapter(PersonAdapter());
+  await Hive.openBox<Person>('person_box');
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
